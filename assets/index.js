bbbdc475eb0813e965cb2647c91006be334d16af
Cristian David Gutierrez <cdgutierrez456@gmail.com>
2022-01-31 20:57:28 -0500

    Agregando plugin para reproduccion automatica. Agregando boton para silenciar o no el video

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,12 @@
 import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
 
 const video = document.querySelector("video")
 const button = document.querySelector("button")
-const player = new MediaPlayer({el: video});
+const buttonMute = document.querySelector(".muted")
+const player = new MediaPlayer({
+    el: video, plugins: [new AutoPlay()]
+});
 
 button.onclick = () => player.togglePlay();
+buttonMute.onclick = () => player.toggleMuted();
